4ef68bc9ef0bfe1014f3e532b06c4f32416f8857
Ali-Aliev-pr <72982603+Ali-Aliev-pr@users.noreply.github.com>
2023-07-15 23:29:43 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const play = document.querySelector('.about__play')
+const video = document.querySelector('.about__video video');
+
+console.log(play)
+// console.log(video)
+
+
+
+play.addEventListener('click', () => {
+  video.play();
+  video.setAttribute('controls', 'controls');
+  play.classList.add('.about__play--hidden');
+})
